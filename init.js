16568de735c618eec1b96597128a349b512b50ab
Tumon <tumon@debian.Tumon>
2022-06-23 21:50:33 +0600

    init

--- init.js
+++ init.js
@@ -1,4 +1,6 @@
 (function(){
-    const con = document.getElementById('country-code');
-    con.style.filter="blur(2px)"
+    const id = document.getElementById('country-code');
+    const cls = document.getElementsByClassName("style-scope ytd-topbar-logo-renderer");
+    id.style.filter="blur(2px)"
+    cls.style.filter="blur(2px)"
 })()
